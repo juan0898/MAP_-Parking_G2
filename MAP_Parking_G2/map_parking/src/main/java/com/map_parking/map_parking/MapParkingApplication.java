4cda262310ceec37468730c2155d765bc9cd20a3
Juan Diego Mora Alvarado <jdmora-2021a@corhuila.edu.co>
2025-03-04 07:07:42 -0500

    fix: folders

---
+++ MAP_Parking_G2/map_parking/src/main/java/com/map_parking/map_parking/MapParkingApplication.java
@@ -0,0 +1,13 @@
+package com.map_parking.map_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapParkingApplication.class, args);
+	}
+
+}
---
+++ MAP_Parking_G2/map_parking/src/test/java/com/map_parking/map_parking/MapParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map_parking.map_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
